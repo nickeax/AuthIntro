6063b923f459eb1a7daad3aabf0b1d3c5c17cac3
Nick Fletcher <nickeax@gmail.com>
2021-09-26 09:03:15 +1000

    Add Claim("Department", "HR") to claims for identity.

---
+++ AuthIntro/Pages/Account/AccessDenied.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace AuthIntro.Pages.Account
+{
+    public class AccessDeniedModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
--- AuthIntro/Pages/Account/Login.cshtml.cs
+++ AuthIntro/Pages/Account/Login.cshtml.cs
@@ -27,7 +27,8 @@ namespace AuthIntro.Pages.Account
                 var claims = new List<Claim>()
                 {
                     new Claim(ClaimTypes.Name, "admin"),
-                    new Claim(ClaimTypes.Email, "admin@gmail.com")
+                    new Claim(ClaimTypes.Email, "admin@gmail.com"),
+                    new Claim("Department", "HR")
                 };
 
                 var identity = new ClaimsIdentity(claims, "MyCookieAuth");
